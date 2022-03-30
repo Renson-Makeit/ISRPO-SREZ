7450cd8546e6ed28efeee9f44a02b54b4f21208a
Your Name <you@example.com>
2022-03-30 12:42:14 +0300

    Добавьте файлы проекта.

---
+++ SlepouKot/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlepouKot
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chequeWord_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chequeExcel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void reportWord_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void reportExcel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
